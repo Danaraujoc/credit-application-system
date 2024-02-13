701f124c86a61b84d35d8570dc3fa90a4e2261b7
Danaraujoc <daniel.araujox77@gmail.com>
2024-02-13 13:17:21 -0300

    Documentando e Testando sua API Rest com Kotlin

--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/dto/request/CustomerUpdateDto.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/dto/request/CustomerUpdateDto.kt
@@ -7,14 +7,14 @@ import java.math.BigDecimal
 
 data class CustomerUpdateDto(
     @field:NotEmpty(message = "Entrada invalida") val firstName: String,
-    @field:NotEmpty(message = "Entrada invalida") val latName: String,
+    @field:NotEmpty(message = "Entrada invalida") val lastName: String,
     @field:NotNull(message = "Entrada invalida") val income: BigDecimal,
     @field:NotEmpty(message = "Entrada invalida") val zipCode: String,
     @field:NotEmpty(message = "Entrada invalida") val street: String
 ) {
     fun toEntity(customer: Customer): Customer {
         customer.firstName = this.firstName
-        customer.lastName = this.latName
+        customer.lastName = this.lastName
         customer.income = this.income
         customer.address.street = this.street
         customer.address.zipCode = this.zipCode
